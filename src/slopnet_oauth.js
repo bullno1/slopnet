50617876c7c468719b24dc17d25b0c050d200635
Bach Le <bach@bullno1.com>
2025-12-24 18:56:18 +0800

    Simplify OAuth implementation code for Emscripten

--- src/slopnet_oauth.js
+++ src/slopnet_oauth.js
@@ -2,46 +2,35 @@
 addToLibrary({
 	$snet_oauth_init__postset: 'snet_oauth_init();',
 	$snet_oauth_init: () => {
-		class Lib {
-			constructor() {
-				this.data = null;
-				this.state = 0;
-				this._onMessage = this.onMessage.bind(this);
-			}
+		let data = null;
+		let state = 0;
 
-			begin(url) {
-				this.data = null;
-				this.state = 0;
-				window.addEventListener("message", this._onMessage);
-				window.open(UTF8ToString(url) + "?origin=" + window.origin);
-			}
-
-			update() {
-				return this.state;
-			}
-
-			data_size() {
-				return this.data ? this.data.length * 4 + 1 : 0;
-			}
-
-			copy_data(ptr, max_size) {
-				if (this.data) {
-					stringToUTF8(this.data, ptr, max_size);
-				}
-			}
-
-			onMessage(message) {
-				this.state = message.data.success == "1" ? 1 : 2;
-				this.data = message.data.data;
-				window.removeEventListener("message", this._onMessage);
-			}
+		const onMessage = (message) => {
+			state = message.data.success == "1" ? 1 : 2;
+			data = message.data.data;
+			window.removeEventListener("message", onMessage);
 		}
 
-		const lib = new Lib();
-		_snet_oauth_begin_js = lib.begin.bind(lib);
-		_snet_oauth_update = lib.update.bind(lib);
-		_snet_oauth_data_size = lib.data_size.bind(lib);
-		_snet_oauth_copy_data = lib.copy_data.bind(lib);
+		_snet_oauth_begin_js = (url) => {
+			data = null;
+			state = 0;
+			window.addEventListener("message", onMessage);
+			window.open(UTF8ToString(url) + "?origin=" + window.origin);
+		};
+
+		_snet_oauth_update = () => {
+			return state;
+		};
+
+		_snet_oauth_data_size = () => {
+			return data ? data.length * 4 + 1 : 0;
+		}
+
+		_snet_oauth_copy_data = (ptr, max_size) => {
+			if (data) {
+				stringToUTF8(data, ptr, max_size);
+			}
+		}
 	},
 	snet_oauth_begin_js: () => {},
 	snet_oauth_begin_js__deps: ['$snet_oauth_init'],
